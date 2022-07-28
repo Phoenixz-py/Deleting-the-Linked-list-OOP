6343feee12870871169610fc0dd574dece548cc3
Fenguin Phoenix <73833580+Phoenixz-py@users.noreply.github.com>
2022-07-28 21:50:47 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 import PromptSync from "prompt-sync";
+
 const prompt = PromptSync({sigint: true})
 class Node {
   constructor(data, next = null) {
@@ -6,7 +7,6 @@ class Node {
     this.next = next;
   }
 }
-
 class List {
   constructor(head = null) {
     this.head = head
@@ -39,7 +39,6 @@ class List {
 
     while (current !== null) {
       yield current.data
-
       current = current.next;
     }
   }
@@ -50,25 +49,31 @@ let askerString = "Enter value you want to make it present in the linked list: "
   let list = new List().append(+prompt(askerString)).append(+prompt(askerString)).append(+prompt(askerString)).append(+prompt(askerString));
 // let list = new List().append(3).append(4).append(6).append(8);
 
-console.log(list)
 console.log([...list])
 
-const askerDel = 'Enter 1 to delete first node data' + '\nEnter 2 to delete the next node' + '\nEnter 3 to delete the 3rd node' + '\nEnter 4 to delete the 4th node: '
-const del = +prompt(askerDel)
+let del = 'What value you want to delete: '
+const delAsker = +prompt(del)
 
-if(del === 1){
+console.log(list)
+
+//custom inputs so far
+
+if(delAsker == 2){
  
-  list.head.data = 0
+  list.head.data === 0
 }
-else if(del === 2){
+else if(delAsker === 3){
 
+  list.head.next.data = 0
 }
-else if(del == 3){
-  list.head.next.data.next.data = 0
+else if(delAsker === 4){
+  list.head.next.next.data = 0
 }
-else if(del === 4){
-  list.head.next.data.next.data.next.node = 0
+else if(delAsker === 5){
+  list.head.next.next.next.data = 0
 }
+
+
 console.log([...list])
 
 
